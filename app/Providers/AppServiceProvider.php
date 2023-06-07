760999c98f5b91df142f15450bae0411153b393d
geobas <ksenera@yahoo.com>
2023-06-07 13:32:57 +0300

    Bind interfaces to concrete implementations

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Contracts\Service\CustomerServiceContract;
+use App\Contracts\Service\OrderServiceContract;
+use App\Services\CustomerService;
+use App\Services\OrderService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +27,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        /**
+         * Bind 'customer service contract' to a concrete implementation class.
+         * In that way we can switch different implementations of 'customer service'
+         * at run-time, without updating the code where 'customer service contract'
+         * is injected.
+         */
+        $this->app->bind(CustomerServiceContract::class, function () {
+            return new CustomerService;
+        });
+
+        $this->app->bind(OrderServiceContract::class, function () {
+            return new OrderService;
+        });
     }
 }
